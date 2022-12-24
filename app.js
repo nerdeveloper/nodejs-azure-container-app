cf3cab694eacdf3eba118fc0f576c30a39083ba8
Obinna Odirionye <odirionye@gmail.com>
2022-12-24 20:24:41 +0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+var  = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
